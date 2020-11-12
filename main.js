76b29065aeb8f55a534ea2723a0201e824914be2
ngochung207 <ngochung207@gmail.com>
2020-11-12 21:51:06 +0700

    tim so lon nhat

---
+++ main.js
@@ -0,0 +1,10 @@
+var myInfo = {
+    name: 'Hung Trinh Ngoc',
+    age: 18,
+    address: 'Ha Noi, Vn',
+    getName: function(){
+        return this.name;
+    }
+};
+var myKey = 'address';
+console.log(myInfo.getName());
